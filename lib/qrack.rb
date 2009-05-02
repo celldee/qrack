a507aa6f9569ba551d6193ff6d43a443c29a64d9
Chris Duncan <celldee@gmail.com>
2009-05-02 18:17:07 +0100

    Initial push

---
+++ lib/qrack.rb
@@ -0,0 +1,12 @@
+$: << File.expand_path(File.dirname(__FILE__))
+
+module Qrack
+  # Qrack version number
+  VERSION = '0.0.1'
+
+  # Return the Qrack version
+  def self.version
+    VERSION
+  end
+
+end
